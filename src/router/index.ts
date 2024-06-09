dfca498bb323e6ca06b44adc01b06c05418a42a5
Monique <moniblig@iafrica.com>
2024-06-09 11:19:38 +0300

    router - prioritise saved position over hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,11 +4,11 @@ import HomeView from '@/views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   scrollBehavior(to, from, savedPosition) {
-    if (to.hash) {
+    if (savedPosition) {
+      return savedPosition
+    } else if (to.hash) {
       const navBar = document.querySelector('#nav') as HTMLElement
       return { el: to.hash, top: navBar?.offsetHeight || 0, behavior: 'smooth' }
-    } else if (savedPosition) {
-      return savedPosition
     }
 
     if (from && Object.keys(to.query).length) {
