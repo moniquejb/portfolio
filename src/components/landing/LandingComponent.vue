b731dc75a303299faea036b636cfa23e043b6297
Monique <moniblig@iafrica.com>
2024-06-09 11:12:51 +0300

    fix incorrect magnet reset on mobile

--- src/components/landing/LandingComponent.vue
+++ src/components/landing/LandingComponent.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bg-light flex flex-wrap">
     <div
-      class="w-full xl:w-4/6 min-h-[400px] xs:min-h-[450px] xl:min-h-[650px] h-[calc(60vh-theme('spacing.20'))] sm:h-[calc(75svh-theme('spacing.20'))] xl:h-[calc(100svh-theme('spacing.24'))] flex justify-center xl:items-center"
+      class="w-full xl:w-4/6 min-h-[400px] xs:min-h-[450px] xl:min-h-[650px] h-[calc(60vh-theme('spacing.20'))] sm:h-[calc(75vh-theme('spacing.20'))] xl:h-[calc(100vh-theme('spacing.24'))] flex justify-center xl:items-center"
     >
       <div
-        class="rounded-3xl min-h-[400px] xs:min-h-[450px] xl:min-h-[600px] h-[calc(60vh-theme('spacing.20'))] sm:h-[calc(75svh-theme('spacing.20'))] xl:h-[calc(90svh-theme('spacing.24'))] w-full pt-4 xl:pt-0 xl:-mt-3"
+        class="rounded-3xl min-h-[400px] xs:min-h-[450px] xl:min-h-[600px] h-[calc(60vh-theme('spacing.20'))] sm:h-[calc(75vh-theme('spacing.20'))] xl:h-[calc(90vh-theme('spacing.24'))] w-full pt-4 xl:pt-0 xl:-mt-3"
       >
         <div class="grid grid-cols-12 grid-rows-12 h-full">
           <div class="col-span-1"></div>
@@ -24,7 +24,7 @@
       </div>
     </div>
     <div
-      class="w-full xl:w-2/6 xl:min-h-[650px] xl:h-[calc(100svh-theme('spacing.24'))] flex justify-center flex-col xl:-ms-8 px-8 xl:px-0 py-8 xs:py-12 xl:py-0"
+      class="w-full xl:w-2/6 xl:min-h-[650px] xl:h-[calc(100vh-theme('spacing.24'))] flex justify-center flex-col xl:-ms-8 px-8 xl:px-0 py-8 xs:py-12 xl:py-0"
     >
       <h1 class="heading-main text-center">Frontend Software Developer</h1>
 
